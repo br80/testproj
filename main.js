b478e7f2491562e6381268c8f8c1f4829bf9c5fe
br80 <bradyfukumoto@gmail.com>
2014-02-28 03:16:42 -0800

    BR80-Have a list of faces

--- main.js
+++ main.js
@@ -3,3 +3,34 @@ function Main($scope)
     $scope.message = "Hello!"
 
 }
+
+
+function StudentsCtrl($scope)
+{
+	$scope.students = [
+        {
+                "name":"Eric"
+        },
+        {
+                "name":"David"
+        },
+        {
+                "name":"Karen"
+        },
+        {
+                "name":"Ashley"
+        },
+        {
+                "name":"Josh"
+        },
+        {
+                "name":"Angelina"
+        }
+	];
+
+	$scope.getNumStudents = function ()
+	{
+		return $scope.students.length;
+	}
+}
+
