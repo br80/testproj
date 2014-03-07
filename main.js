f7f8ffaae93354e688a93006611b0b713032b149
br80 <bradyfukumoto@gmail.com>
2014-03-07 02:43:36 -0800

    BR80-Added modal dialogue code and CSS

--- main.js
+++ main.js
@@ -2,6 +2,35 @@ var app = angular.module("myApp", []);
 
 
 
+
+app.directive('modalDialog', function() {
+  return {
+    restrict: 'E',
+    scope: {
+      show: '='
+    },
+    replace: true, // Replace with the template below
+    transclude: true, // we want to insert custom content inside the directive
+    link: function(scope, element, attrs) {
+      scope.dialogStyle = {};
+      if (attrs.width)
+        scope.dialogStyle.width = attrs.width;
+      if (attrs.height)
+        scope.dialogStyle.height = attrs.height;
+      scope.hideModal = function() {
+        scope.show = false;
+      };
+    },
+    template: "<div class='ng-modal' ng-show='show'><div class='ng-modal-overlay' ng-click='hideModal()'></div><div class='ng-modal-dialog' ng-style='dialogStyle'><div class='ng-modal-close' ng-click='hideModal()'>X</div><div class='ng-modal-dialog-content' ng-transclude></div></div></div>"
+  };
+});
+
+
+
+
+
+
+
 app.controller('ClassroomCtrl', ['$scope', '$timeout', function($scope,$timeout)
 {
 	// Dictionary with game and player variables
@@ -9,6 +38,12 @@ app.controller('ClassroomCtrl', ['$scope', '$timeout', function($scope,$timeout)
 		"turn":0,
 		"name":"Brady"	
 	}
+	
+	$scope.modalShown = false;
+	$scope.toggleModal = function() {
+    	$scope.modalShown = !$scope.modalShown;
+	};
+	
 	
 	// Array with students
 	$scope.students = [
